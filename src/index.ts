e54d4981f0a538b9899bd0117b471ce6cf90c164
Sergei Dyshel <sergei@dyshel.com>
2024-11-28 23:24:47 +0200

    feat: convert to CJS
    Long live CJS!

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export default {}
+import Index from './commands/autocomplete'
+import Create from './commands/autocomplete/create'
+import Script from './commands/autocomplete/script'
+
+export const commands = {
+  autocomplete: Index,
+  'autocomplete:create': Create,
+  'autocomplete:script': Script,
+}
+
+export {default as hook} from './hooks/refresh-cache'
