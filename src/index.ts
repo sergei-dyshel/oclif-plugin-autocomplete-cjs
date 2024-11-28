fd0fc67d9bd9107f12296489a14344e3120b2060
Sergei Dyshel <sergei@dyshel.com>
2024-11-28 23:24:47 +0200

    feat: export needed symbols

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export default {}
+import Create from './commands/autocomplete/create.js'
+import Index from './commands/autocomplete/index.js'
+import Script from './commands/autocomplete/script.js'
+
+export const commands = {
+  autocomplete: Index,
+  'autocomplete:create': Create,
+  'autocomplete:script': Script,
+}
+
+export {default as hook} from './hooks/refresh-cache.js'
